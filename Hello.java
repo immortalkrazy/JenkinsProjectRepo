3445949a3f952eff61071dd9ad34805897e40007
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-04-20 16:42:35 -0400

    added loop in Hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World.......");
+        for (int i = 1; i < 10; 1++) {
+            System.out.println("Hello World......." + i);
+        }
     }
 }
